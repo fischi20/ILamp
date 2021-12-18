0775c5e5910e81e8edbfd4a21b7af9b3731645e1
Daniel.Fischnaller <DaFischnaller@unibz.it>
2021-12-18 10:11:31 +0000

    load config from config file

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import json
-import pathlib
+import sys
+from src.config_loader import load_config
 
-config = None
+config = load_config(__file__)
 
-# load config from file
-try:
-    with open('config2.json', 'r') as configfile:
-        config = json.load(configfile)
-except FileNotFoundError as e:
-    readme = pathlib.Path(__file__).parent.joinpath('README.md').resolve()
-    print(f"Couldn't find config.json file, please check out the {readme}")
-
-# If the config file doesn't exist the user is required to set it up
 if not config:
-    quit()
-
-# Do stuff with config here
-print(config)
+    sys.exit()
