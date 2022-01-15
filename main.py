21beb710d6c103b9960e3f7bf9bae133c0fea4e3
Daniel.Fischnaller <DaFischnaller@unibz.it>
2022-01-15 21:19:41 +0000

    standard color + commented out sense stuff

--- main.py
+++ main.py
@@ -1,11 +1,62 @@
-from sense_hat import SenseHat
+# from sense_hat import SenseHat
 from time import sleep
 import sys
 from Adafruit_IO import Client, MQTTClient
 from src.config_loader import load_config
-from src.adafruit import create_clients, setup_MQTTClient, get_client
+from src.adafruit import create_clients, setup_MQTTClient, get_client, get_aio_client
 
+config = load_config(__file__)
+if not config: # exits if no config was found
+    sys.exit()
 
+feeds = config.get("feeds") # feeds dictionary
+feed_list = list()
+for(k, value) in feeds.items(): #creates an array of all the feeds for mqtt client
+    feed_list.append(value)
+
+#* globals
+color = (0, 0, 0)
+
+# sense = SenseHat()
+# sense.clear()
+# sense.low_light = config.get('low_light_mode') or False
+
+def get_feed_id(feed_name):
+    """
+    Returns the feed id of the specified feed name
+    """
+    return aio_feeds.get(feed_name)
+
+def get_last(feed_name):
+    """
+    Returns the last value of the specified feed
+    """
+    return aio.receive_previous(get_feed_id(feed_name)).value
+
+def hex_to_rgb(value):
+    """
+    Converts a hex value to an RGB tuple
+
+    Argument:
+    value - hex value
+    """
+    value = value.lstrip('#')
+    return tuple(int(value[i:i+2], 16) for i in (0, 2, 4))
+
+def set_color(new_color):
+    """
+    Sets the color of the light
+
+    Argument:
+    color - RGB tuple representing a color or hex value
+    """
+    global color
+    if(type(new_color) == str):
+        color = hex_to_rgb(new_color)
+    
+    color = new_color
+    
+    #updateLight(sense, color)
 
 def updateLight(sense, color):
     """
@@ -29,33 +80,43 @@ def alarmMode(sense):
     red = (255, 0, 0)
     while True:
         updateLight(sense, red)
-        sleep(.5)
+        sleep(1)
         sense.clear()
 
 def feed_handler(client, feed_id, payload):
     """
     Prints the new value every time one of the specified feeds has a new value
     """
-    print(f"{feed_id}: {payload}")
+    if(feed_id == feeds.get("light_toggle")):
+        if(payload == 'True'):
+            print("Light on")
+        else:
+            # sense.clear()
+            print("Light off")
+            
+    if(feed_id == feeds.get("light_color")): #handles color change
+        print(f"New color: {hex_to_rgb(payload)}")
 
-config = load_config(__file__)
-
-if not config: # exits if no config was found
-    sys.exit()
-
-sense = SenseHat()
-sense.clear()
-sense.low_light = config.get('low_light_mode') or False
 
 create_clients(config.get("Adafruit_IO_username"), config.get("Adafruit_IO_key"))
-setup_MQTTClient(feed_handler, config.get('feeds'))
+setup_MQTTClient(feed_handler, feed_list)
 client = get_client()
+aio = get_aio_client()
+
+# Setup feed list for ids
+aio_feeds = dict()
+for f in aio.feeds():
+    print(f.name)
+    aio_feeds[f.name] = f.id
 
 client.connect()
+
+set_color(get_last("LightColor")) # load default color from adafruit_io
 
 # loops to check for new data
 client.loop_background()
 
+"""
 color = (255,255,141) # mild yellow
 room_temperature = 20
 
@@ -78,3 +139,4 @@ while True:
 
     sense.clear(color)
     sleep(5) # sleep for n seconds
+"""
