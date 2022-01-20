fc9f9e0f634dfbd4d095d2bbed499ca6406369c8
Daniel.Fischnaller <DaFischnaller@unibz.it>
2022-01-20 11:42:44 +0000

    LerpColor configUpdates

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ for(k, value) in feeds.items(): #creates an array of all the feeds for mqtt clie
 
 #* globals
 color = (0, 0, 0)
+secondary_color = (0, 0, 0)
 light_on = False
-
+min_temp = config.get("min_temp")
+max_temp = config.get("max_temp")
 # sense = SenseHat()
 # sense.clear()
 # sense.low_light = config.get('low_light_mode') or False
@@ -33,21 +35,41 @@ def hex_to_rgb(value):
     value = value.lstrip('#')
     return tuple(int(value[i:i+2], 16) for i in (0, 2, 4))
 
-def set_color(new_color):
+def set_color(new_color, primary_color = True):
     """
     Sets the color of the light
 
     Argument:
     color - RGB tuple representing a color or hex value
+    primary_color - boolean, True if the color is the primary color, False if it is the secondary color
     """
-    global color
-    if(type(new_color) == str):
-        color = hex_to_rgb(new_color)
+    global color, secondary_color
+    if type(new_color) == str:
+        if primary_color == True:
+            color = hex_to_rgb(new_color)
+        else:
+            secondary_color = hex_to_rgb(new_color)
     
-    color = new_color
-    if(light_on):
-        #updateLight(sense, color)
-        print("Light color: " + str(color))
+    if primary_color == True:
+        color = new_color
+    else:
+        secondary_color = new_color
+
+def lerpColor(cold, hot, t):
+    """
+    Linearly interpolates between two colors
+
+    Arguments:
+    color1 - RGB tuple representing a color
+    color2 - RGB tuple representing a color
+    t - float between 0 and 1
+    """
+    r1, g1, b1 = cold
+    r2, g2, b2 = hot
+    r = int(r1 + (r2 - r1) * t)
+    g = int(g1 + (g2 - g1) * t)
+    b = int(b1 + (b2 - b1) * t)
+    return (r, g, b)
 
 def set_light(state):
     """
@@ -58,10 +80,6 @@ def set_light(state):
     """
     global light_on
     light_on = state
-    if(light_on):
-        set_color(color)
-    #else:
-    #  sense.clear()   
 
 def updateLight(sense, color):
     """
@@ -73,21 +91,6 @@ def updateLight(sense, color):
         for y in range(8):
             sense.set_pixel(x,y, color)
 
-
-# doesn't work
-def alarmMode(sense):
-    """
-    A mode which in case of emergency makes SenseHat blink
-
-    Argument:
-    sense - senseHat
-    """
-    red = (255, 0, 0)
-    while True:
-        updateLight(sense, red)
-        sleep(1)
-        sense.clear()
-
 def feed_handler(client, feed_id, payload):
     """
     Prints the new value every time one of the specified feeds has a new value
@@ -96,11 +99,31 @@ def feed_handler(client, feed_id, payload):
         if(payload == 'True'):
             print("Light on")
         else:
-            # sense.clear()
             print("Light off")
             
     if(feed_id == feeds.get("light_color")): #handles color change
+        print("primary color changed to: " + payload)
         set_color(payload)
+
+    if(feed_id == feeds.get("secondary_color")):
+        print("secondary color changed to: " + payload)
+        set_color(payload, False)
+
+def calcColorPercent(min, max, temp):
+    """
+    Calculates the percentage of the color range that the temperature is in
+
+    Arguments:
+    min - minimum temperature
+    max - maximum temperature
+    temp - temperature
+    """
+    tp = (temp-min)/(max-min)
+    if tp > 1:
+      tp = 1
+    if tp < 0:
+      tp = 0
+    return tp
 
 
 create_clients(config.get("Adafruit_IO_username"), config.get("Adafruit_IO_key"))
@@ -109,34 +132,26 @@ client = get_client()
 
 client.connect()
 
+#get last values
 set_light(get_last(feeds.get("light_toggle"))) # load the state of the light
+set_color(get_last(feeds.get("secondary_color")), False) # load the color of the light
 set_color(get_last(feeds.get("light_color"))) # load default color from adafruit_io
-
 
 # loops to check for new data
 client.loop_background()
 
-"""
-color = (255,255,141) # mild yellow
-room_temperature = 20
 
 while True:
-    temp = sense.get_temperature(); # getting current temperature
-    print(temp) 
+    """
+    temp = sense.get_temperature()
 
-    if temp < room_temperature:
-        color = (160, 219, 255)
+    upload_data(feeds.get("temp"), temp) # uploads the temperature to adafruit_io
+    upload_data(feeds.get("humidity"), sense.get_humidity()) # uploads the humidity to adafruit_io
 
-    if temp >= room_temperature:
-        if temp < room_temperature + 2:
-            color = (254, 253, 172)
+    if light_on:
+        # calc color
+        display_color = lerpColor(secondary_color, color, calcColorPercent(min_temp, max_temp, temp))
 
-        elif temp < room_temperature + 4:
-            color = (255, 205, 0)
-
-        else:
-            color = (227, 147, 0) # dark orange 
-
-    sense.clear(color)
-    sleep(5) # sleep for n seconds
-"""
+        sense.clear(display_color)
+    """
+    sleep(1) # sleep for n seconds
